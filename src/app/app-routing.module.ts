b4692ae50bf1909977e4fd91f4e1ad080897977b
unknown <tadakamallasanthoshini@gmail.com>
2020-03-16 21:25:50 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {Child1Component} from './child1/child1.component';
+import {Child2Component} from './child2/child2.component';
+import{AppComponent} from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'',component:AppComponent,
+},
+{
+  path:'tollywood',component:Child1Component
+},
+{
+  path:'bollywood',component:Child2Component
+}
+]
+;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
